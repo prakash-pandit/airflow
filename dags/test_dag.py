6f4b104106d5268b00720abd4a356f7494744045
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-06-01 18:23:23 +0530

    dag addition

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+from airflow import DAG
+
 default_args = {
     schedule_interval = '0 7 * * *',
-    tags = ['test_dag']
+    start_date = datetime.strptime('2024-05-31', 'YYYY-MM-dd'),
+    tags = ['test_dag'],
+    catchup = True
 }
+
+dag = DAG(
+    default_args = default_args,
+)
 
 from exampledag import examples_astronauts
